7e7f19542ce4d2d9e64bff7c9a4e3aa5db55d7c4
Elephant200 <alexktev@gmail.com>
2025-06-29 12:12:23 -0400

    Fix name of dependency

---
+++ main.py
@@ -0,0 +1,21 @@
+# Order orphans.json by alphabetical order of orphan name
+
+# Currently on Kainuk
+
+import json
+
+with open("orphans.json", "r") as f:
+    orphans = json.load(f)
+
+# Sort orphans by name
+orphans = dict(sorted(orphans.items(), key=lambda x: x[0]))
+
+print(orphans)
+
+def get_progress(orphan_name: str) -> int:
+    """
+    Given an orphan name, return how many images, up to and including that orphan (in the alphebetized list), exist using the orphans dict
+    """
+    return sum(orphans[key] for key in orphans.keys() if key <= orphan_name)
+
+print(get_progress("kainuk"))
