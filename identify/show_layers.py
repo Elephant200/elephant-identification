4065a7d14201f56f7c7ec956c17a463d47c5c466
Alex <alexktev@gmail.com>
2025-09-19 19:09:14 -0400

    Display activation function details

--- identify/show_layers.py
+++ identify/show_layers.py
@@ -17,6 +17,8 @@ for i, layer in enumerate(feature_extractor.layers):
     except Exception as e:
         print(f"{"Couldn't get shape":<20}", end="")
     print(f"(Type: {str(type(layer).__name__)})", end="")
+    if type(layer) == keras.layers.Activation:
+        print(layer)
     print()
 all_layers = [layer.name for layer in model.layers]
 print(f"Number of layers: {len(all_layers)}")
